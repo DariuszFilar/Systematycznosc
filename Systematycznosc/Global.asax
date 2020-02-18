a6de536e5e014e8b663f6261adb2bbd39ec6c95b
darek.filar <darek.filar@gmail.com>
2020-02-18 18:03:08 +0100

    Add project files.

---
+++ Systematycznosc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Systematycznosc.MvcApplication" Language="C#" %>
---
+++ Systematycznosc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Systematycznosc.Startup))]
+namespace Systematycznosc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
