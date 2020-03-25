2436f172f63a4cb22925b9ec621315dce7253834
darek.filar <darek.filar@gmail.com>
2020-03-25 18:54:11 +0100

    Instaled JqueryUI

--- Systematycznosc/App_Start/BundleConfig.cs
+++ Systematycznosc/App_Start/BundleConfig.cs
@@ -13,6 +13,8 @@ namespace Systematycznosc
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+   "~/Scripts/jquery-ui-{version}.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
@@ -25,6 +27,10 @@ namespace Systematycznosc
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+"~/Content/themes/base/jquery.ui.core.css",
+"~/Content/themes/base/jquery.ui.autocomplete.css",
+"~/Content/themes/base/jquery.ui.theme.css"));
         }
     }
 }
